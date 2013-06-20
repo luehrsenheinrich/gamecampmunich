0bd92b6c2dd3ee9da5731c4733041ff3c67fce73
Hendrik Luehrsen <hendrik@luehrsen.de>
2013-06-20 17:01:01 +0200

    Twitwall Sponsoren

--- wp-content/themes/gcmuc/page-twitwall.php
+++ wp-content/themes/gcmuc/page-twitwall.php
@@ -10,12 +10,15 @@ get_header(); ?>
 <div id="content" class="clearfix twitter">
 
 	<div id="sponsoren">
-    	<img src="http://gcmuc.luehrsen-heinrich.de/wp-content/themes/gcmuc/img/sponsoren/travian.jpg">
-        <img src="http://gcmuc.luehrsen-heinrich.de/wp-content/themes/gcmuc/img/sponsoren/microsoft.jpg">
-        <img src="http://gcmuc.luehrsen-heinrich.de/wp-content/themes/gcmuc/img/sponsoren/google.jpg" />
-    	<img src="http://gcmuc.luehrsen-heinrich.de/wp-content/themes/gcmuc/img/sponsoren/mdh.jpg" style="width: 120px; height: auto">
-        <img src="http://gcmuc.luehrsen-heinrich.de/wp-content/themes/gcmuc/img/sponsoren/bneun.jpg" >
-        <img src="http://gcmuc.luehrsen-heinrich.de/wp-content/themes/gcmuc/img/sponsoren/cipsoft.jpg" />
+        
+<img src="http://www.gamecampmunich.de/wp-content/themes/gcmuc/img/sponsoren/gamesbavaria.jpg" alt="Games/Bavaria">
+<img src="http://www.gamecampmunich.de/wp-content/themes/gcmuc/img/sponsoren/microsoft.jpg" alt="Microsoft">
+<img src="http://www.gamecampmunich.de/wp-content/themes/gcmuc/img/sponsoren/travian.jpg" alt="Travian Games">
+<img src="http://www.gamecampmunich.de/wp-content/themes/gcmuc/img/sponsoren/cipsoft.png" alt="Cipsoft">
+<img src="http://www.gamecampmunich.de/wp-content/themes/gcmuc/img/sponsoren/sae.png" alt="SAE Institute">
+<img src="http://www.gamecampmunich.de/wp-content/themes/gcmuc/img/sponsoren/mdh.png" alt="Mediadesign Hochschule">
+<img src="http://www.gamecampmunich.de/wp-content/themes/gcmuc/img/sponsoren/ravensburger.jpg" alt="Ravensburger Digital">
+
     </div>
 
 	<div id="twitter_wall">
