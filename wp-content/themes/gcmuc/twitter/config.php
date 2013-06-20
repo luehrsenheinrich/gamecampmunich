f2687906bdbc53ee4e1a10e8df73501f6e889687
Hendrik Luehrsen <hendrik@luehrsen.de>
2013-06-20 18:04:03 +0200

    Remove Junk

--- wp-content/themes/gcmuc/twitter/config.php
+++ wp-content/themes/gcmuc/twitter/config.php
@@ -7,13 +7,7 @@
 
 //get public directory structure eg "/top/second/third" 
 $public_directory = 'http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['PHP_SELF']); 
-//place each directory into array 
-$directory_array = explode('/', $public_directory); 
-//get highest or top level in array of directory strings 
-$public_base = max($directory_array); 
 
 define('CONSUMER_KEY', 'Xn9ZsjwjJtjBaReEMX1veg');
 define('CONSUMER_SECRET', 'AEcHkzrNz2vNvayBYzW4gaVUfdMKlqOxlT74YZueN0');
 define('OAUTH_CALLBACK', $public_directory."/callback.php");
-
-var_dump(OAUTH_CALLBACK);
