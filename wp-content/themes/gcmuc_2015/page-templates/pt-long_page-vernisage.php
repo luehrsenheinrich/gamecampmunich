da73821b0634d114d203ea27dc9b2982562a9ab6
Christopher Kanitz <ck@luehrsen-heinrich.de>
2015-04-30 16:06:33 +0200

    custom header for vernisage

---
+++ wp-content/themes/gcmuc_2015/page-templates/pt-long_page-vernisage.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Template Name: Long Page Vernisage
+ */
+
+define("IN_LONG_PAGE", true);
+$_longpage_elements = array();
+
+get_header("vernisage");
+?>
+
+
+<div class="long_page_wrapper">
+	<?php
+	// Build the query for child pages
+	$args = array(
+		'post_parent'	=> get_the_ID(),
+		'post_type'		=> 'page',
+		'orderby'		=> 'menu_order date',
+		'order'			=> 'ASC',
+		'posts_per_page'=> -1,
+	);
+	$query = new WP_Query( $args );
+
+	if($query->have_posts()): while ( $query->have_posts() ) : $query->the_post();
+		// We will load the custom templates now
+
+
+		$page_template = get_post_meta($post->ID, "_wp_page_template", true);
+		if(preg_match("#(content-templates/ct-)#i", $page_template) && file_exists(trailingslashit(WP_THEME_PATH).$page_template)){
+			$_longpage_elements[] = $post;
+		}
+
+	endwhile; endif;
+
+	$query->rewind_posts();
+	
+	if($query->have_posts()): while ( $query->have_posts() ) : $query->the_post();
+		// We will load the custom templates now
+
+
+		if(in_array($post, $_longpage_elements)){
+			$page_template = get_post_meta($post->ID, "_wp_page_template", true);
+			$current_index = array_search($post, $_longpage_elements);
+			include(trailingslashit(WP_THEME_PATH).$page_template);
+		}
+
+	endwhile; endif;
+	?>
+</div>
+<?php
+	get_footer();
+?>
