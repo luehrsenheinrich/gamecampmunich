56ded84ff6cea07cb730648f7fa623d4a21e3f71
Volker <vh@luehrsen-heinrich.de>
2015-05-07 10:32:46 +0200

    newsletter signup fix

--- wp-content/themes/gcmuc_2015/content-templates/ct-social.php
+++ wp-content/themes/gcmuc_2015/content-templates/ct-social.php
@@ -28,7 +28,7 @@ $style_tag = pickerStyles($post);
 
 ?>
 
-<div <? post_class("ct-wrapper ct-content clearfix"); ?> id="<?=$post->post_name?>" <?php echo $style_tag; ?>> 
+<div <? post_class("ct-wrapper ct-content ct-social clearfix"); ?> id="<?=$post->post_name?>" <?php echo $style_tag; ?>> 
 	<div class="container">
 		<div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
 			<h1><?php the_title(); ?></h1>
@@ -43,25 +43,30 @@ $style_tag = pickerStyles($post);
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 					<!-- Begin MailChimp Signup Form -->
-					<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
-					<style type="text/css">
-						#mc_embed_signup{clear:left; font:14px Helvetica,Arial,sans-serif; }
-						/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-						   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-					</style>
-					<div id="mc_embed_signup">
-					<form action="//gamecampmunich.us4.list-manage.com/subscribe/post?u=5d3e6f58ed4d30ea452c04f3e&amp;id=8f10ee5f62" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-					    <div id="mc_embed_signup_scroll">
-						<label for="mce-EMAIL">GameCamp Newsletter</label>
-						<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-					    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-					    <div style="position: absolute; left: -5000px;"><input type="text" name="b_5d3e6f58ed4d30ea452c04f3e_8f10ee5f62" tabindex="-1" value=""></div>
-					    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-					    </div>
-					</form>
-					</div>
-					
-					<!--End mc_embed_signup-->
+						<div id="mc_embed_signup">
+						<form action="//gamecampmunich.us4.list-manage.com/subscribe/post?u=5d3e6f58ed4d30ea452c04f3e&amp;id=8f10ee5f62" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" novalidate>
+						    <div id="mc_embed_signup_scroll">
+							<h2>GameCamp Munich Newsletter</h2>
+						<div class="mc-field-group form-group">
+								<input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL" placeholder="E-Mail *">
+						</div>
+						<div class="mc-field-group form-group">
+							<input type="text" value="" name="FNAME" class="required form-control" id="mce-FNAME" placeholder="Vorname *">
+						</div>
+						<div class="mc-field-group form-group">
+							<input type="text" value="" name="LNAME" class="form-control" id="mce-LNAME" placeholder="Name">
+						</div>
+							<div id="mce-responses" class="clear">
+								<div class="response" id="mce-error-response" style="display:none"></div>
+								<div class="response" id="mce-success-response" style="display:none"></div>
+							</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+						    <div style="position: absolute; left: -5000px;"><input type="text" name="b_5d3e6f58ed4d30ea452c04f3e_8f10ee5f62" tabindex="-1" value=""></div>
+						    <div class="clear submit-btn"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button btn"></div>
+						    </div>
+						</form>
+						</div>
+						
+						<!--End mc_embed_signup-->
 				</div>
 			</div>
 		</div>
