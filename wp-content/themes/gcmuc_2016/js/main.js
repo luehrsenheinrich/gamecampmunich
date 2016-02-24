73875786be09060732a1872624a60f21fd76b0e0
Christopher Kanitz <ck@luehrsen-heinrich.de>
2016-02-24 18:33:33 +0100

    new style.css header + font change

--- wp-content/themes/gcmuc_2016/js/main.js
+++ wp-content/themes/gcmuc_2016/js/main.js
@@ -1,5 +1,18 @@
 // @depend "plugins.js"
 
+WebFontConfig = {
+	google: { families: [ 'Open+Sans:400,700,400italic,600,600italic,700italic:latin' ] }
+};
+
+(function() {
+	var wf = document.createElement('script');
+	wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+})();
+
 jQuery(document).ready(function($) {
 	$(".nav-trigger").click(function(){
 		$("body").toggleClass("nav_open");
