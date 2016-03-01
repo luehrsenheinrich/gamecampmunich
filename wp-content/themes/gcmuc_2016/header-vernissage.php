a6897200c07fd5554723792833b0b8b5e2c816e3
Christopher Kanitz <ck@luehrsen-heinrich.de>
2016-03-01 22:10:56 +0100

    vernissage branding

---
+++ wp-content/themes/gcmuc_2016/header-vernissage.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title><?php wp_title(); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript">
+    	// Check if JavaScript is available
+	    document.documentElement.className =
+	       document.documentElement.className.replace("no-js","js");
+	</script>
+
+    <?php wp_head(); ?>
+
+</head>
+<body <?php body_class( 'vernissage' ); ?>>
+    <div class="viewport" id="top">
+        <div class="page-wrapper">
+            <header class="page-header">
+                <div class="container">
+                    <div class="row"> 
+                        <!--<div class="logo"></div>-->
+                        <div class="mask" style="position:absolute;left:0;top:0;right:0;bottom:0;height:100%;">
+                            <img src="<?php echo WP_THEME_URL; ?>/img/mask.svg">
+                        </div>
+                        <img src="<?php echo get_option('header_logo'); ?>">
+                        <!--<div class="claim"></div>-->
+                    </div>
+                </div>
+            </header>
+            <div class="main-nav-wrapper container">
+                <div class="nav-btn-wrapper">
+                    <?php
+                        $args = array(
+                                "theme_location"    => "main_nav",
+                                'menu_class'        => 'menu clearfix main',
+                                'container'         => 'nav',
+                                'container_class'   => 'main-menu',
+                                'fallback_cb'       => false,
+                                'depth'             => 1
+                        );
+                        wp_nav_menu($args);
+                    ?>
+                    <div class="hidden-md hidden-lg nav-trigger-wrapper">
+                        <span class="nav-trigger">
+                            <i class="fa fa-bars"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
---
+++ wp-content/themes/gcmuc_2016/page_templates/pt-one_page_vernissage.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template Name: One Page Master
+ */
+
+define("IN_LONG_PAGE", true);
+$_longpage_elements = array();
+global $post;
+
+get_header('vernissage');
+
+?>
+
+<div class="one-page-wrapper">
+	<?php
+	// Build the query for child pages with content templates
+	$args = array(
+		'post_parent'	=> get_the_ID(),
+		'post_type'		=> 'page',
+		'orderby'		=> 'menu_order date',
+		'order'			=> 'ASC',
+		'posts_per_page'=> -1,
+		'meta_key'		=> '_lh_content_template',
+	);
+	$query = new WP_Query( $args );
+	$current_index = 0;
+	if($query->have_posts()): while ( $query->have_posts() ) : $query->the_post();
+		// We will load the custom templates now
+		$page_template = get_post_meta($post->ID, "_lh_content_template", true); // Get the template file path (relative to the theme root)
+
+		if(file_exists(trailingslashit(WP_THEME_PATH).$page_template)){ // Check if the template file really exists
+			load_template(trailingslashit(WP_THEME_PATH).$page_template, false); // Load the template file
+			$current_index++; // Interate the index
+		}
+	endwhile; endif;
+	?>
+</div>
+
+<?php
+	get_footer();
+?>
