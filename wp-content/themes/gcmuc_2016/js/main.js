0c6b26911f66602ed334b760efa51b5068714520
Christopher Kanitz <ck@luehrsen-heinrich.de>
2016-03-02 23:07:44 +0100

    activate lightbox

--- wp-content/themes/gcmuc_2016/js/main.js
+++ wp-content/themes/gcmuc_2016/js/main.js
@@ -1,4 +1,5 @@
 // @depend "plugins.js"
+// @depend "../bower_components/magnific-popup/dist/jquery.magnific-popup.min.js";
 
 WebFontConfig = {
 	google: { families: [ 'Open+Sans:400,700,400italic,600,600italic,700italic:latin' ] }
@@ -29,5 +30,34 @@ jQuery(document).ready(function($) {
 				$('.nav-btn-wrapper').removeClass('fixed');
 			}
 		}, 5);
+	});
+
+	// Initialization of Magnific Popup
+	$("a[href*='.png']").addClass("image-link");
+	$("a[href*='.gif']").addClass("image-link");
+	$("a[href*='.jpg']").addClass("image-link");
+	$("a[href*='.jpeg']").addClass("image-link");
+
+
+	$('.image-link').magnificPopup({
+		type:'image',
+		image: {
+			titleSrc: function(item){
+				return $(item.el).parents(".gallery-item").find(".gallery-caption").text();
+			}
+		},
+		gallery: {
+			enabled: true, // set to true to enable gallery
+
+			preload: [0,2], // read about this option in next Lazy-loading section
+
+			navigateByImgClick: true,
+
+			arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+			tPrev: lh.prev, // title for left button
+			tNext: lh.next, // title for right button
+			tCounter: lh.counter // markup of counter
+		}
 	});
 })
